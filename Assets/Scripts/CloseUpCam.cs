b1fa3a7c8dce90e5c83d306875aa60cd56cccec5
Ulf R. Pedersen <ulf@urp.dk>
2017-01-20 22:49:40 +0100

    Add Follow cams

---
+++ Assets/Scripts/CloseUpCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseUpCam : MonoBehaviour {
+
+    [SerializeField]
+    private Transform m_CameraPosition;
+
+	// Use this for initialization
+	void Start () {
+        transform.SetParent(m_CameraPosition);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
