41f28bb81bedf258c792045c1dad0877409c2b16
abrp <abrp@abrp.dk>
2017-01-21 09:58:26 +0100

    added some managers

---
+++ Assets/Scripts/CustomMonobehavior.cs
@@ -0,0 +1,60 @@
+﻿//==============================================================================
+// 
+//==============================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomMonobehavior : MonoBehaviour {
+
+    //==============================================================================
+    // MonoBehaviour
+    //==============================================================================
+
+    //==============================================================================
+    // Fields
+    //==============================================================================
+    private void Update () {
+        switch (GameManager.instance.GetCurrentGameState)
+        {
+            case GameManager.GameState.Pre:
+                PreLoop();
+                break;
+            case GameManager.GameState.Game:
+                GameLoop();
+                break;
+            case GameManager.GameState.Pause:
+                PauseLoop();
+                break;
+            case GameManager.GameState.End:
+                EndLoop();
+                break;
+            default:
+                PauseLoop();
+                    break;
+        }
+    }
+
+    //==============================================================================
+    // Game Loops
+    //==============================================================================
+
+    protected virtual void PreLoop() {
+
+    }
+
+    protected virtual void GameLoop(){
+
+    }
+
+    protected virtual void PauseLoop(){
+
+    }
+
+    protected virtual void EndLoop()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //==============================================================================
+    // Fields
+    //==============================================================================
+
+    public static GameManager instance = null;
+
+    public enum GameState {Pre, Game, End, Pause};
+
+    private GameState m_CurrentGameState = GameState.Pre;
+
+    //==============================================================================
+    // Properties
+    //==============================================================================
+
+    public GameState GetCurrentGameState {
+        get { return m_CurrentGameState;  }
+    }
+
+    //==============================================================================
+    // MonoBehaviour
+    //==============================================================================
+
+    //==============================================================================
+    public void Awake()
+    {
+        SetupSingleton();
+    }
+
+    //==============================================================================
+    private void SetupSingleton()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    //==============================================================================
+    public void SetGameState(GameState newGameState) {
+        m_CurrentGameState = newGameState;
+    }
+}
