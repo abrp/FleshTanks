5637386a9573aceec1643c686dbeae951f4f0e8b
AudiologMusic <fuad@vonloops.com>
2017-01-22 14:46:46 +0100

    fucked animations

---
+++ Assets/Scripts/DoorAnnouncement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnnouncement : MonoBehaviour {
+
+
+    [SerializeField]
+    private AudioClip m_Close;
+
+    private AudioSource m_AudioSouce;
+
+    private void Start() {
+        m_AudioSouce = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void Open() {
+        CenterTextManager.instance.StartTextType("Gates are Open!", true);
+    }
+
+    public void Close() {
+        m_AudioSouce.clip = m_Close;
+        m_AudioSouce.Play();
+        CenterTextManager.instance.StartTextType("Gates are Closing!",true);
+    }
+
+
+	
+}
