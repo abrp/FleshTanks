37c83cb971caa568a6fbc2dcb8d10e34e57e6790
AudiologMusic <fuad@vonloops.com>
2017-01-22 10:57:58 +0100

    UI push

---
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour {
+
+    //==============================================================================
+    // Fields
+    //==============================================================================
+
+    [SerializeField]
+    private Text m_Lives;
+    [SerializeField]
+    private Text m_Score;
+
+    public void UpdateLives(int lives) {
+        m_Lives.text = lives.ToString();
+    }
+
+    public void UpdateScore(int score) {
+        m_Score.text = score.ToString();
+    }
+    
+
+}
